09eb552d14c392a903947955e32594f58bda9f1b
Durvasahuji <74409509+Durvasahuji@users.noreply.github.com>
2021-07-01 13:40:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function perload
+{
+     
+}
+
+function draw
+{
+    image(video,0,0,300,300);
+}
+
+function setup
+{
+    creat=createCanvas(300,300);
+    create=createCapture(VIDEO);
+    video.hide();
+}
